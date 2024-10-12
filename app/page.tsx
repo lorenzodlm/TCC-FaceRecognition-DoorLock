895c620b7f3e1bd1c66a1c4c9bda2a3d26470e61
lorenzodlm <145830734+lorenzodlm@users.noreply.github.com>
2024-10-12 17:31:59 +0700

    Added new screenshot

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function HomePage() {
       <footer className="py-6 md:py-0 border-t border-border/40">
         <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
           <p className="text-balance text-center text-sm leading-loose text-muted-foreground">
-            Built on top of{" "}
+            Built with{" "}
             <Link
               href="https://ui.shadcn.com"
               target="_blank"
