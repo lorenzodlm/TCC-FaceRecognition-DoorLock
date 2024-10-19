804d1d4623c15cf293392e766cff39029ca0f259
lorenzodlm <145830734+lorenzodlm@users.noreply.github.com>
2024-10-19 20:51:40 +0700

    Updated TOS

--- app/info/terms/page.tsx
+++ app/info/terms/page.tsx
@@ -115,6 +115,86 @@ export default function AboutPage() {
                 </p>
                 </div>
 
+                <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+                SECTION 8 - ERRORS, INACCURACIES AND OMISSIONS
+                </h1>
+                <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
+                <p className="text-center text-lg font-light text-foreground">
+                Occasionally there may be information on our site or in the Service that contains typographical errors, inaccuracies or omissions that may relate to product descriptions, pricing, promotions, offers, product shipping charges, transit times and availability. We reserve the right to correct any errors, inaccuracies or omissions, and to change or update information or cancel orders if any information in the Service or on any related website is inaccurate at any time without prior notice (including after you have submitted your order). We undertake no obligation to update, amend or clarify information in the Service or on any related website, including without limitation, pricing information, except as required by law. No specified update or refresh date applied in the Service or on any related website, should be taken to indicate that all information in the Service or on any related website has been modified or updated.                
+                </p>
+                </div>
+
+                <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+                SECTION 8 - PROHIBITED USES
+                </h1>
+                <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
+                <p className="text-center text-lg font-light text-foreground">
+                In addition to other prohibitions as set forth in the Terms of Service, you are prohibited from using the site or its content: (a) for any unlawful purpose; (b) to solicit others to perform or participate in any unlawful acts; (c) to violate any international, federal, provincial or state regulations, rules, laws, or local ordinances; (d) to infringe upon or violate our intellectual property rights or the intellectual property rights of others; (e) to harass, abuse, insult, harm, defame, slander, disparage, intimidate, or discriminate based on gender, sexual orientation, religion, ethnicity, race, age, national origin, or disability; (f) to submit false or misleading information; (g) to upload or transmit viruses or any other type of malicious code that will or may be used in any way that will affect the functionality or operation of the Service or of any related website, other websites, or the Internet; (h) to collect or track the personal information of others; (i) to spam, phish, pharm, pretext, spider, crawl, or scrape; (j) for any obscene or immoral purpose; or (k) to interfere with or circumvent the security features of the Service or any related website, other websites, or the Internet. We reserve the right to terminate your use of the Service or any related website for violating any of the prohibited uses.
+                </p>
+                </div>
+
+                <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+                SECTION 9 - DISCLAIMER OF WARRANTIES; LIMITATION OF LIABILITY
+                </h1>
+                <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
+                <p className="text-center text-lg font-light text-foreground">
+                We do not guarantee, represent or warrant that your use of our Service will be uninterrupted, timely, secure or error-free. We do not warrant that the results that may be obtained from the use of the Service will be accurate or reliable. You agree that from time to time we may remove the Service for indefinite periods of time or cancel the Service at any time, without notice to you. You expressly agree that your use of, or inability to use, the Service is at your sole risk. The Service and all products and Services delivered to you through the Service are (except as expressly stated by us) provided 'as is' and 'as available' for your use, without any representation, warranties or conditions of any kind, either express or implied, including all implied warranties or conditions of merchantability, merchantable quality, fitness for a particular purpose, durability, title, and non-infringement. In no case shall AfterFall, our directors, officers, employees, affiliates, agents, contractors, interns, suppliers, Service providers or licensors be liable for any injury, loss, claim, or any direct, indirect, incidental, punitive, special, or consequential damages of any kind, including, without limitation lost profits, lost revenue, lost savings, loss of data, replacement costs, or any similar damages, whether based in contract, tort (including negligence), strict liability or otherwise, arising from your use of any of the Service or any products procured using the Service, or for any other claim related in any way to your use of the Service or any product, including, but not limited to, any errors or omissions in any content, or any loss or damage of any kind incurred as a result of the use of the Service or any content (or product) posted, transmitted, or otherwise made available via the Service, even if advised of their possibility. Because some states or jurisdictions do not allow the exclusion or the limitation of liability for consequential or incidental damages, in such states or jurisdictions, our liability shall be limited to the maximum extent permitted by law.
+                </p>
+                </div>
+
+                <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+                SECTION 10 - INDEMNIFICATION
+                </h1>
+                <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
+                <p className="text-center text-lg font-light text-foreground">
+                You agree to indemnify, defend and hold harmless AfterFall and our parent, subsidiaries, affiliates, partners, officers, directors, agents, contractors, licensors, Service providers, subcontractors, suppliers, interns and employees, harmless from any claim or demand, including reasonable attorneys’ fees, made by any third-party due to or arising out of your breach of these Terms of Service or the documents they incorporate by reference, or your violation of any law or the rights of a third-party.
+                </p>
+                </div>
+
+                <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+                SECTION 11 - SEVERABILITY
+                </h1>
+                <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
+                <p className="text-center text-lg font-light text-foreground">
+                In the event that any provision of these Terms of Service is determined to be unlawful, void or unenforceable, such provision shall nonetheless be enforceable to the fullest extent permitted by applicable law, and the unenforceable portion shall be deemed to be severed from these Terms of Service, such determination shall not affect the validity and enforceability of any other remaining provisions.
+                </p>
+                </div>
+
+                <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+                SECTION 12 - TERMINATION
+                </h1>
+                <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
+                <p className="text-center text-lg font-light text-foreground">
+                The obligations and liabilities of the parties incurred prior to the termination date shall survive the termination of this agreement for all purposes. These Terms of Service are effective unless and until terminated by either you or us. You may terminate these Terms of Service at any time by notifying us that you no longer wish to use our Services, or when you cease using our site. If in our sole judgment you fail, or we suspect that you have failed, to comply with any term or provision of these Terms of Service, we also may terminate this agreement at any time without notice and you will remain liable for all amounts due up to and including the date of termination; and/or accordingly may deny you access to our Services (or any part thereof).
+                </p>
+                </div>
+
+                <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+                SECTION 13 - ENTIRE AGREEMENT
+                </h1>
+                <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
+                <p className="text-center text-lg font-light text-foreground">
+                The failure of us to exercise or enforce any right or provision of these Terms of Service shall not constitute a waiver of such right or provision. These Terms of Service and any policies or operating rules posted by us on this site or in respect to the Service constitutes the entire agreement and understanding between you and us and governs your use of the Service, superseding any prior or contemporaneous agreements, communications and proposals, whether oral or written, between you and us (including, but not limited to, any prior versions of the Terms of Service). Any ambiguities in the interpretation of these Terms of Service shall not be construed against the drafting party.
+                </p>
+                </div>
+
+                <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+                SECTION 14 - GOVERNING LAW
+                </h1>
+                <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
+                <p className="text-center text-lg font-light text-foreground">
+                These Terms of Service and any separate agreements whereby we provide you Services shall be governed by and construed in accordance with the laws of Thailand.
+                </p>
+                </div>
+
+                <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-5xl lg:leading-[1.1]">
+                SECTION 15 - CHANGES TO TERMS OF SERVICE
+                </h1>
+                <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
+                <p className="text-center text-lg font-light text-foreground">
+                You can review the most current version of the Terms of Service at any time at this page. We reserve the right, at our sole discretion, to update, change or replace any part of these Terms of Service by posting updates and changes to our website. It is your responsibility to check our website periodically for changes. Your continued use of or access to our website or the Service following the posting of any changes to these Terms of Service constitutes acceptance of those changes.
+                </p>
+                </div>
 
             </section>
             </div>
